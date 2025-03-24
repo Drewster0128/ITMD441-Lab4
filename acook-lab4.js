d33a68db1c17182410c465d701eea7a223da1ce8
Drewster0128 <acook21@hawk.iit.edu>
2025-03-24 14:33:31 -0500

    Change subheading in hero

--- acook-lab4.js
+++ acook-lab4.js
@@ -5,4 +5,8 @@
 
     //1. Change the main headline to "Uplift Your Brand with Stellar Marketing"
     document.getElementsByTagName("h1")[0].innerText = "Uplift Your Brand with Stellar Marketing";
+
+    //2. Change the line of text below the hero headline to the following test
+    //Utilize cutting-edge strategies from Stellar Marketing to help your business thrive and excel
+    document.getElementsByTagName("p")[0].innerHTML = 'Utilize cutting-edge strategies from Stellar Marketing to help your business <b style="font-style: italic;">thrive</b> and <b style="font-style: italic;">excel</b>';
 })();
