6af55d7a4a3a7f679396cabaf5a26db3e4eeb079
Drewster0128 <acook21@hawk.iit.edu>
2025-03-24 14:21:07 -0500

    Change headline in hero

--- acook-lab4.js
+++ acook-lab4.js
@@ -2,5 +2,7 @@
 // ITMD441 Undergrade Student
 
 (function () {
-    
+
+    //1. Change the main headline to "Uplift Your Brand with Stellar Marketing"
+    document.getElementsByTagName("h1")[0].innerText = "Uplift Your Brand with Stellar Marketing";
 })();
