cde247f04250faa7b7fbe7f02a5dcd21c0a2cb92
Drewster0128 <acook21@hawk.iit.edu>
2025-03-31 09:28:29 -0500

    Split chances 1 and 2 to their own IFEE function

--- acook-lab4.js
+++ acook-lab4.js
@@ -1,12 +1,15 @@
 // Andrew Cook
 // ITMD441 Undergrade Student
 
-(function () {
 
-    //1. Change the main headline to "Uplift Your Brand with Stellar Marketing"
+//1. Change the main headline to "Uplift Your Branch with Stellar Marketing"
+(function() {
     document.getElementsByTagName("h1")[0].innerText = "Uplift Your Brand with Stellar Marketing";
-
-    //2. Change the line of text below the hero headline to the following test
-    //Utilize cutting-edge strategies from Stellar Marketing to help your business thrive and excel
-    document.getElementsByTagName("p")[0].innerHTML = 'Utilize cutting-edge strategies from Stellar Marketing to help your business <b style="font-style: italic;">thrive</b> and <b style="font-style: italic;">excel</b>';
 })();
+
+//2. Change the line of text below the hero headline to the following test
+    //Utilize cutting-edge strategies from Stellar Marketing to help your business thrive and excel
+(function() {
+    document.getElementsByTagName("p")[0].innerHTML = 'Utilize cutting-edge strategies from Stellar Marketing to help your business <b style="font-style: italic;">thrive</b> and <b style="font-style:italic;">excel</b>'
+})();
+
